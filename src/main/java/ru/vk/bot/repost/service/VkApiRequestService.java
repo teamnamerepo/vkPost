9c20f86297ce48d5cdcce1da4a918eb4f0b3cfc7
WebJavaLion <lev-2222@yandex.ru>
2019-10-27 12:41:31 +0300

    video

--- src/main/java/ru/vk/bot/repost/service/VkApiRequestService.java
+++ src/main/java/ru/vk/bot/repost/service/VkApiRequestService.java
@@ -20,6 +20,8 @@ import ru.vk.bot.repost.repository.VkPostRepository;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 /**
@@ -37,6 +39,9 @@ public class VkApiRequestService {
     @Autowired
     VkPostRepository postRepository;
 
+    private final static String regExp = "^.*(https://m\\.youtube|https://www\\.twitch\\.tv/|https://youtu\\.be/).*$";
+    private final static Pattern pattern = Pattern.compile(regExp);
+
     private static final int targetGroupId = -79268570;
 
     @Transactional
@@ -51,7 +56,7 @@ public class VkApiRequestService {
                     .ownerId(targetGroupId)
                     .execute();
             List<WallpostFull> postListFromResponse = response.getItems();
-
+            System.out.println(postListFromResponse);
 
             List<VkPost> existingListFromDb = postRepository.findByVkIdIn(postListFromResponse.stream()
                     .map(WallpostFull::getId)
@@ -59,52 +64,59 @@ public class VkApiRequestService {
             );
 
             postListFromResponse.stream()
-                    .filter(post -> !post.getText().contains("http://") && !post.getText().contains("https://")
+                    .filter(post -> pattern.matcher(post.getText()).matches() || (
+                            !post.getText().contains("http://") && !post.getText().contains("https://"))
                     )
-                    .filter(post -> !post.getAttachments().stream().map(wallPost -> wallPost.getType().getValue())
-                            .collect(Collectors.toList()).contains("video")
-                    ).forEach(post -> {
-                        if (!existingListFromDb.stream().map(VkPost::getVkId).collect(Collectors.toList()).contains(post.getId())) {
-
-                            VkPost postForBd = new VkPost();
-                            List<VkAttachment> attachmentsForBd = new ArrayList<>();
-
-                            postForBd.setVkId(post.getId());
-                            postForBd.setText(post.getText());
-                            postForBd.setIsSent(false);
-                            postForBd.setDate(new Timestamp(post.getDate() * 1000L));
-
-                            List<WallpostAttachment> attachments = post.getAttachments();
-                            if (!CollectionUtils.isEmpty(attachments)) {
-
-                                for (WallpostAttachment attachmentFromResponse : attachments) {
-                                    VkAttachment attachmentForBd = new VkAttachment();
-
-                                    if ("photo".equals(attachmentFromResponse.getType().getValue())) {
-                                        Photo photo = attachmentFromResponse.getPhoto();
-
-                                        for (PhotoSizeEnum size : PhotoSizeEnum.values()) {
-
-                                            if (attachmentForBd.getUrl() == null) {
-                                                photo.getSizes().stream()
-                                                        .filter(photoSizes -> size.getValue().equals(photoSizes.getType().getValue())
-                                                        )
-                                                        .findAny()
-                                                        .ifPresent(photoSizes -> attachmentForBd.setUrl(photoSizes.getUrl().getPath())
-                                                );
-                                            } else {
-                                                break;
-                                            }
-                                        }
-                                    }
-                                    attachmentForBd.setPost(postForBd);
-                                    attachmentsForBd.add(attachmentForBd);
+                    .filter(post -> {
+                                if (post.getAttachments().stream().map(wallPost -> wallPost.getType().getValue())
+                                        .collect(Collectors.toList()).contains("video")) {
+                                    return pattern.matcher(post.getText()).matches();
+                                } else {
+                                    return true;
                                 }
                             }
-                            postForBd.setAttachments(attachmentsForBd);
-                            postsForBd.add(postForBd);
+                    ).forEach(post -> {
+                if (!existingListFromDb.stream().map(VkPost::getVkId).collect(Collectors.toList()).contains(post.getId())) {
+
+                    VkPost postForBd = new VkPost();
+                    List<VkAttachment> attachmentsForBd = new ArrayList<>();
+
+                    postForBd.setVkId(post.getId());
+                    postForBd.setText(post.getText());
+                    postForBd.setIsSent(false);
+                    postForBd.setDate(new Timestamp(post.getDate() * 1000L));
+
+                    List<WallpostAttachment> attachments = post.getAttachments();
+                    if (!CollectionUtils.isEmpty(attachments)) {
+
+                        for (WallpostAttachment attachmentFromResponse : attachments) {
+                            VkAttachment attachmentForBd = new VkAttachment();
+
+                            if ("photo".equals(attachmentFromResponse.getType().getValue())) {
+                                Photo photo = attachmentFromResponse.getPhoto();
+
+                                for (PhotoSizeEnum size : PhotoSizeEnum.values()) {
+
+                                    if (attachmentForBd.getUrl() == null) {
+                                        photo.getSizes().stream()
+                                                .filter(photoSizes -> size.getValue().equals(photoSizes.getType().getValue())
+                                                )
+                                                .findAny()
+                                                .ifPresent(photoSizes -> attachmentForBd.setUrl(photoSizes.getUrl().getPath())
+                                                );
+                                    } else {
+                                        break;
+                                    }
+                                }
+                            }
+                            attachmentForBd.setPost(postForBd);
+                            attachmentsForBd.add(attachmentForBd);
                         }
-                    });
+                    }
+                    postForBd.setAttachments(attachmentsForBd);
+                    postsForBd.add(postForBd);
+                }
+            });
 
         } catch (ApiException | ClientException e) {
             e.printStackTrace();
